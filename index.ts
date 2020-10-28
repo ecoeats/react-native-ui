2f1a7abccc68f73a28d58ba46a1bd6f9e69bf306
Stewart McGown <stewartcallummcgown@gmail.com>
2020-10-28 14:16:01 +0000

    fix: default export

--- index.ts
+++ index.ts
@@ -1,9 +1,4 @@
-import Button from './src/Button';
-import Text from './src/Text';
-import { Touchable } from './src/Touchable';
-
-export default {
-  Button,
-  Text,
-  Touchable,
-};
+export { default as Button } from './src/Button';
+export { useUI } from './src/context/UIContext';
+export { default as Text } from './src/Text';
+export { Touchable } from './src/Touchable';
