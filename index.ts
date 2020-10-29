c5ce92627179fc1b57f66ca0f4b8a9f7088b16ab
Stewart McGown <stewartcallummcgown@gmail.com>
2020-10-29 12:34:34 +0000

    fix: export all

--- index.ts
+++ index.ts
@@ -4,4 +4,5 @@ export { UIProvider, useUI } from './src/context/UIContext';
 export { Radio } from './src/Radio';
 export { default as Text } from './src/Text';
 export { Touchable } from './src/Touchable';
+export * from './src/utils/postcodes-api';
 export { shadow } from './src/utils/shadow';
